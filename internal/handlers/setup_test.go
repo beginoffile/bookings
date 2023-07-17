f1a42e47280beafaf8955f18edad0d850e603f82
beginoffile <cursos.mapatupisaro@gmail.com>
2023-07-16 22:04:10 -0600

    sending mail finished

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -45,6 +45,13 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 
 	if err != nil {
@@ -62,6 +69,14 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
