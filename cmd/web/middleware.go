853b9c70e79e89575d7c2f544adec30fcf167cc9
beginoffile <cursos.mapatupisaro@gmail.com>
2023-07-30 22:41:01 -0600

    deploy production

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -33,7 +33,7 @@ func SessionLoad(next http.Handler) http.Handler {
 
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !helpers.IsAuthenticated(r) {
+		if !helpers.IsAuthenticated(r) || 1 == 1 {
 			session.Put(r.Context(), "error", "Login in first!")
 			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
